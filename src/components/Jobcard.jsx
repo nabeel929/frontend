b6af5e7503314d3ee9cee0401dbc8a299b43a63e
nabeel929 <57416512+nabeel929@users.noreply.github.com>
2025-08-05 16:04:29 +0500

    jobs html isssue

--- src/components/Jobcard.jsx
+++ src/components/Jobcard.jsx
@@ -6,8 +6,8 @@ const JobCard = ({ job }) => {
       <div className="card-body">
         <h5 className="card-title">{job.title}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{job.company}</h6>
-        <p className="card-text">{job.location}</p>
-        <div>
+        <p className="card-text">{job.location}</p> 
+        <div> 
           <button className="btn btn-outline-primary btn-sm me-2">Edit</button>
           <button className="btn btn-outline-danger btn-sm">Delete</button>
         </div>
